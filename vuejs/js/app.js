1402b5ec55948f49791768b31ea8d56783214278
Giovanni Gauditano <giovanni@webbingbrasil.com.br>
2017-09-18 14:14:19 -0300

    exercise 3

--- vuejs/js/app.js
+++ vuejs/js/app.js
@@ -1,15 +1,46 @@
 new Vue({
-   el: '#app',
-   data: {
-       title: 'Teste de titulo',
-       link: 'http://matheusggds.github.io'
-   },
-   methods: {
-       getTitle: function() {
-           return this.title;
-       },
-       changeLink: function(event) {
-           this.link = event.target.value;
-       }
-   }
-});
+  el: "#exercise_1",
+  data: {
+    name: 'Matheus Gomide',
+    age: '23',
+    imageSource: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMH1aUUTraVl2DmfszcMMi3yBpHKpCH6_vn76XQr0Yx71SXB0dh8Z7mzk'
+  },
+  methods: {
+    getRandom: function() {
+      return Math.random() * (1 - 0) + 0;
+    }
+  }
+})
+
+new Vue({
+  el: "#exercise_2",
+  data: {
+      value: 'Old value'
+  },
+  methods: {
+      alert: function() {
+          alert('Funcionando');
+      }
+  }
+})
+
+new Vue({
+  el: "#exercise_3",
+  data: {
+      value: 0
+  },
+    computed: {
+      result: function() {
+        return this.value == 37 ? 'done' : 'not there yet'
+      }
+    },
+  watch: {
+    result: function() {
+      var vm = this;
+
+      setTimeout(function() {
+        vm.value = 0;
+      }, 5000);
+    }
+  }
+})
