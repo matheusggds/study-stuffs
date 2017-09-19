830b2c95819106d4334e6db3df29b0ff81775d6b
Giovanni Gauditano <giovanni@webbingbrasil.com.br>
2017-09-19 14:50:00 -0300

    basics 2

--- vuejs/basics_1/js/app.js
+++ vuejs/basics_1/js/app.js
@@ -6,7 +6,7 @@ new Vue({
     imageSource: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMH1aUUTraVl2DmfszcMMi3yBpHKpCH6_vn76XQr0Yx71SXB0dh8Z7mzk'
   },
   methods: {
-    getRandom: function() {
+    getRandom: function () {
       return Math.random() * (1 - 0) + 0;
     }
   }
@@ -15,32 +15,47 @@ new Vue({
 new Vue({
   el: "#exercise_2",
   data: {
-      value: 'Old value'
+    value: 'Old value'
   },
   methods: {
-      alert: function() {
-          alert('Funcionando');
-      }
+    alert: function () {
+      alert('Funcionando');
+    }
   }
 })
 
 new Vue({
   el: "#exercise_3",
   data: {
-      value: 0
+    value: 0
   },
-    computed: {
-      result: function() {
-        return this.value == 37 ? 'done' : 'not there yet'
-      }
-    },
+  computed: {
+    result: function () {
+      return this.value == 37 ? 'done' : 'not there yet'
+    }
+  },
   watch: {
-    result: function() {
+    result: function () {
       var vm = this;
 
-      setTimeout(function() {
+      setTimeout(function () {
         vm.value = 0;
       }, 5000);
+    }
+  }
+})
+
+new Vue({
+  el: "#exercise_4",
+  data: {
+    effectClasses: {
+      highlight: false,
+      shrink: true
+    }
+  },
+  methods: {
+    startEffect: function() {
+      
     }
   }
 })
