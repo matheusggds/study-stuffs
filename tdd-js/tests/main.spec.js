375343f76eee53e2e30bb78d2609f415a0ef61a2
Matheus Gomide <matheusggds@hotmail.com>
2019-03-11 23:58:20 -0300

    feat: main.spec example

---
+++ tdd-js/tests/main.spec.js
@@ -0,0 +1,15 @@
+describe('Main', () => {]
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen blabla', () => {
+        // Espera que aconteça
+        // Entrada de dados / método sum(2,2)
+        // Espera retornar (4) => true OR (3) => false => broken test
+      })
+    })
+  })
+
+  describe('Method B', () => {
+
+  })
+})
